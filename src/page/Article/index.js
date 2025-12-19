7d665420ecb87687ee80a3f908ea4fd7c2bb6133
pjl <1007020896@qq.com>
2025-12-19 15:28:50 +0800

    路由导航跳转

--- src/page/Article/index.js
+++ src/page/Article/index.js
@@ -1,7 +1,27 @@
+/*
+  编程式导航
+      是指通过 `useNavigate` 钩子得到导航方法，然后通过调用方法以命令式的形式进行路由跳转，比如想在登录请求完毕之后跳转就可以选择这种方式，更加灵活
+    代码示例：
+      import { useNavigate } from "react-router-dom"
+      const Login = () => {
+        const navigate = useNavigate()
+        return (
+          <div>
+            我是登录页
+            <button onClick={() => navigate('/article')}>跳转至文章</button>
+          </div>
+        )
+      }
+    语法说明：
+      通过调用navigate方法传入地址path实现跳转
+*/
+import { useNavigate } from "react-router-dom";
 const Article = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>我是Article</h1>
+      <button onClick={() => navigate('/login')}>跳转到login</button>
     </div>
   );
 };
--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,7 +1,17 @@
+/*
+  声明式导航
+      是指通过在模版中通过 `<Link/>` 组件描述出要跳转到哪里去，比如后台管理系统的左侧菜单通常使用这种方式进行
+    代码示例：
+      <Link to="/article">文章</Link>
+    语法说明：
+      通过给组件的to属性指定要跳转到路由path，组件会被渲染为浏览器支持的a链接，如果需要传参直接通过字符串拼接的方式拼接参数即可
+*/
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div>
       <h1>我是Login</h1>
+      <Link to="/article">跳转到article</Link>
     </div>
   );
 };
